3e731c9ac3b2339ffd5acecb57f8b26691b5e859
Leonam Quintão <leonam.quintao@bairesdev.com>
2022-02-07 18:12:51 -0300

    add google map

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+/// <reference types="@types/google.maps" />
 import { User } from './entities/User';
 import { Company } from './entities/Company';
 import { Map } from './entities/Map';
+
+const map = document.getElementById('map_canvas');
+
+const opts = {
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  zoom: 1,
+};
+
+new google.maps.Map(map, opts);
